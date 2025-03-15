1846d469abba2d8ce3b67c9b6b310d23748b02c9
prnv007-rgb <pranavmallia2004@gmail.com>
2025-03-15 13:16:58 +0530

    Added missing files

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 @app.route("/")
+def home():
+    return "Hello World!"
 # -------------------------------
 # Resume Classification Setup
 # -------------------------------
