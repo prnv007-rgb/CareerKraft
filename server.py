d72808f26e15571cbebb14219401d693c8c15595
prnv007-rgb <pranavmallia2004@gmail.com>
2025-03-15 10:53:27 +0530

    Added missing files

--- server.py
+++ server.py
@@ -185,5 +185,5 @@ def generate_feedback():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))  # Default to 10000
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.8080", port=port)
